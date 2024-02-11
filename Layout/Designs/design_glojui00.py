98c4c50affc22eceb1dde0396023478a5e6070c7
glojui <99444713+glojui@users.noreply.github.com>
2024-02-10 19:47:54 -0800

    Update design_glojui00.py

--- Layout/Designs/design_glojui00.py
+++ Layout/Designs/design_glojui00.py
@@ -33,10 +33,10 @@ def design_glojui00(cell, cell_y, inst_wg1, inst_wg2, inst_wg3, waveguide_type):
     # load the cells from the PDK
     # choose appropriate parameters
     cell_bragg = ly.create_cell('ebeam_pcell_bragg_grating', library, {
-        'number_of_periods':60,
+        'number_of_periods':50,
         'grating_period': 0.270,
-        'corrugation_width': 0.08,
-        'wg_width': 0.385,
+        'corrugation_width': 0.05,
+        'wg_width': 0.35,
         'sinusoidal': True})
     if not cell_bragg:
         raise Exception ('Cannot load Bragg grating cell; please check the script carefully.')
